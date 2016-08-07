7f5f8491302ba5923356d1a4e7459e7ac8a075a3
Stephan Avenwedde <s.avenwedde@gmail.com>
2016-08-07 20:51:09 +0200

    word on interface

--- pgs_interface.c
+++ pgs_interface.c
@@ -2,7 +2,7 @@
 
 
 uint8_t connect_db(){
-char *conninfo = "dbname = postgres";
+char *conninfo = "host=localhost port=5432 dbname=mydb user=stephan password=rakete connect_timeout=10";
 PGconn *conn;
 PGresult *res;
 
@@ -12,9 +12,9 @@ conn = PQconnectdb(conninfo);
 if(PQstatus(conn) != CONNECTION_OK){
 printf("\nConnection failed: %s\n", PQerrorMessage(conn));
 exit_nicely(conn);
+} else {
+printf("\nConnected to %s\n", PQdb(conn));
 }
-printf("\nConnected !\n");
-
 }
 
 
